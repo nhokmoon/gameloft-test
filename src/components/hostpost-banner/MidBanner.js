6e4b8de6334815c8c1f9b83f52911e71a659fa16
tuandev <tranminhtuan12an3nh2019@gmail.com>
2023-09-11 08:51:15 +0700

    update

--- src/components/hostpost-banner/MidBanner.js
+++ src/components/hostpost-banner/MidBanner.js
@@ -81,8 +81,8 @@ const MidBanner = () => {
         <p className="mt-4 md:max-w-lg lg:max-w-xl">
           <span className="hidden md:inline">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-            has been the industry's standard dummy text ever since the 1500s, when an unknown
-            printer took a galley of type and scrambled it to make a type specimen book.
+            has been the industry standard dummy text ever since the 1500s, when an unknown printer
+            took a galley of type and scrambled it to make a type specimen book.
           </span>
         </p>
       </div>
